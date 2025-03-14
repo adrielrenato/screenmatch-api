2654a52ffcdfd6bd5fd7a08abc10181c8f64debc
Adriel Renato <adriel.renato26@gmail.com>
2025-03-13 21:14:54 -0300

    Commit inicial para começar o projeto

---
+++ src/main/java/br/com/alura/screenmatch_com_jpa/ScreenmatchComJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch_com_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchComJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchComJpaApplication.class, args);
+	}
+
+}
