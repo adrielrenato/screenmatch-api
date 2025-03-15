97e0310069a5b438ee6666623cb830329e0f0e62
Adriel Renato <adriel.renato26@gmail.com>
2025-03-15 14:11:07 -0300

    Configuração incial do spring web e configuração do container docker

--- src/main/java/br/com/alura/screenmatch/desafio/repository/CategoriaDesafioRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.alura.screenmatch.desafio.repository;
-
-import br.com.alura.screenmatch.desafio.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaDesafioRepository extends JpaRepository<Categoria, Long> {}
